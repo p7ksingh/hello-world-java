604c7a2c62c768a29f4ef6431b0c0e07ac0f32db
Developer <developer@Developers-Mac-mini.local>
2024-03-15 13:59:06 +0530

    hello world project

---
+++ src/main/java/one/ozone/www/HelloWorldJavaApplication.java
@@ -0,0 +1,13 @@
+package one.ozone.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldJavaApplication.class, args);
+	}
+
+}
