0546b660890348eddf7cab79e3301cae60d0ae7a
Developer <developer@Developers-Mac-mini.local>
2024-03-15 14:10:55 +0530

    hello world rest controller added

---
+++ src/main/java/one/ozone/www/HelloWorldBean.java
@@ -0,0 +1,28 @@
+package one.ozone.www;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+}
